a5fcea9ed8121bbc5ee3d247759a7d9dbad499b2
xanimal <zeeb0002@umn.edu>
2019-06-24 11:28:20 -0500

    added static grid plane

--- lab.js
+++ lab.js
@@ -12,24 +12,29 @@ class Lab {
       this.renderer.setSize( window.innerWidth, window.innerHeight );
       document.body.appendChild( this.renderer.domElement );
 
-      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
       const light = new THREE.DirectionalLight( 0xffffff );
       light.position.set( 0, 20, 10 );
       const ambient = new THREE.AmbientLight( 0x707070 ); // soft white light
 
-      const material = new THREE.MeshPhongMaterial( { color: 0x00aaff } );
-
-      this.cube = new THREE.Mesh( geometry, material );
-
-      this.scene.add( this.cube );
       this.scene.add( light );
       this.scene.add( ambient );
+
+      // ground
+  		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20, 20), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+  		mesh.rotation.x = - Math.PI / 2;
+  		mesh.receiveShadow = true;
+  		this.scene.add( mesh );
+
+  		var grid = new THREE.GridHelper( 20, 20, 0x000000, 0x000000 );
+  		grid.material.opacity = 0.2;
+  		grid.material.transparent = true;
+  		this.scene.add( grid );
 
       this.camera.position.z = 3;
     }
 
 	animate() {
-    
+        //for now, just render the scene
         this.renderer.render( this.scene, this.camera );
     }
 }
