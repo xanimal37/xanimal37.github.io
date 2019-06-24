7811779867ce27dc8c1f7baa3334d0c385ee46e3
xanimal <zeeb0002@umn.edu>
2019-06-24 12:16:50 -0500

    controls

--- lab.js
+++ lab.js
@@ -10,6 +10,7 @@ class Lab {
       this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
       this.renderer = new THREE.WebGLRenderer();
       this.renderer.setSize( window.innerWidth, window.innerHeight );
+      this.controls = new THREE.OrbitControls(this.camera,this.renderer.domElement);
       document.body.appendChild( this.renderer.domElement );
 
       const light = new THREE.DirectionalLight( 0xffffff );
@@ -45,6 +46,7 @@ class Lab {
 
 	animate() {
         //for now, just render the scene
+        this.controls.update();
         this.renderer.render( this.scene, this.camera );
     }
 }
