e5256379e48de52b76fbcc5bbaf64e60517cafca
xanimal <zeeb0002@umn.edu>
2019-06-27 14:43:38 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -104,6 +104,7 @@ class Lab {
     //for mouse interaction
     this.raycaster.setFromCamera(this.mouse,this.camera);
     var intersects = this.raycaster.intersectObjects(this.scene.children);
+    console.log(intersects[0].object.name);
     //check for intersection with video screen
     if(intersects[0].object.name=="Screen"){
       this.toggleMovie();
