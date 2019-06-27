fef36a7841a6910e3028b115ef0250ce53d313ba
xanimal <zeeb0002@umn.edu>
2019-06-27 16:02:46 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -16,7 +16,7 @@ class Lab {
     //set up scnee
     this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-      this.renderer = new THREE.WebGLRenderer();
+      this.renderer = new THREE.WebGLRenderer({antialias:true});
       this.renderer.shadowMap.enabled=true; //shadow test
       this.renderer.setSize( window.innerWidth, window.innerHeight );
       this.controls = new THREE.OrbitControls(this.camera,this.renderer.domElement);
