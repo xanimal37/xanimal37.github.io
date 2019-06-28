4bee5f0b79c71b72de7a8934cae02c0e9b8bfdc8
xanimal <zeeb0002@umn.edu>
2019-06-28 13:16:22 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -154,7 +154,7 @@ class Lab {
           //get each model from the scene(that s what we get from the glb file, a scene)
             data.scene.traverse(function(child){
               if(child instanceof THREE.Mesh){
-                child.position.set(0,0,0);
+              
                 child.castShadow=true; //shadow test
                 child.receiveShadow=true; //shadow test
                 lab.meshList.push(child);
