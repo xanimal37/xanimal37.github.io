425c1de1665a4239cbb113a7d354f9071b7fc593
xanimal <zeeb0002@umn.edu>
2019-06-30 16:52:46 -0500

    test2

--- lab.js
+++ lab.js
@@ -138,7 +138,8 @@ class Lab {
     const lab = this;
 
     var assetsToLoad=[
-      "assets/test.glb"
+      "assets/test.glb",
+      "assets/test2.glb"
       //"assets/room.glb",
       //"assets/fixtures.glb",
       //"assets/equipment.glb",
