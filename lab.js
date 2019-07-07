68567fa053c095525e5b2fdb374481129a41efd0
xanimal <zeeb0002@umn.edu>
2019-07-07 18:16:27 -0500

    more animations

--- lab.js
+++ lab.js
@@ -160,6 +160,8 @@ class Lab {
 
           var testMixer = new THREE.AnimationMixer(gltf.scene);
           testMixer.clipAction(gltf.animations[0]).play();
+          testMixer.clipAction(gltf.animations[1]).play();
+          testMixer.clipAction(gltf.animations[2]).play();
           lab.mixers.push(testMixer);
           console.log("There are " + gltf.animations.length + " animations in the scene.");
           });
