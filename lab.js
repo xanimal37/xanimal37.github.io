2abf8f31324629492e2fea592709655e87252c75
xanimal <zeeb0002@umn.edu>
2019-07-08 10:17:41 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -27,15 +27,17 @@ class Lab {
       this.controls = new THREE.OrbitControls(this.camera,this.renderer.domElement);
       document.body.appendChild( this.renderer.domElement );
 
-      const light = new THREE.DirectionalLight( 0xffffff,1 );
-      light.position.set( 2, 2, 2 );
-      light.castShadow=true;
+      const spotlight = new THREE.SpotLight( 0xffffff,1 );
+      //spotlight.position.set( 2, 2, 2 );
+      spotlight.castShadow=true;
+      spotlight.shadow.mapSize.height = 1024;
+      spotlight.shadow.mapsize.width=1024;
       //const light2 = new THREE.DirectionalLight(0xffffff,1);
       //light2.position.set(-3,2,-1);
       //light AxesHelpe
-      const lightHelper = new THREE.DirectionalLightHelper(light,5);
+      const lightHelper = new THREE.DirectionalLightHelper(spotlight,3);
       const ambient = new THREE.AmbientLight( 0xffffff,2);
-      this.scene.add( light );
+      this.scene.add( spotlight );
       //this.scene.add(light2);
       this.scene.add(lightHelper);
       this.scene.add( ambient );
