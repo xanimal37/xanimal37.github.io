aa925e8c95e6cc991ecf1ea40309dd4488ad899a
xanimal <zeeb0002@umn.edu>
2019-07-23 10:21:42 -0500

    Update lab.js

--- lab.js
+++ lab.js
@@ -124,7 +124,7 @@ class Lab {
       this.scene.updateMatrixWorld(); //need or will get coordinates for the whole model
       let test = new THREE.Vector3();
       intersects[0].object.getWorldPosition(test);
-      this.camera.position.set(1,1,0);
+      this.camera.position.set(0.5,1.86,.43);
       this.controls.target=test;
       console.log("move camera target "+ intersects[0].object.name);
     }
