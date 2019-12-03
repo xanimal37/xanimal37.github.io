751437515f249af021d085faebf3d7d73fd3fbad
xanimal <zeeb0002@umn.edu>
2019-12-03 12:13:43 -0600

    imagepick

--- js/PicPicker.js
+++ js/PicPicker.js
@@ -8,11 +8,19 @@ window.onload = function picLoad() {
     items = document.getElementsByClassName("thumb");
     for(var i=0;i<items.length;i++){
       items[i].addEventListener("click",ShowImage);
-    }  
+    }
+
+    document.getElementById("closeModalButton").addEventListener("click",HideImage);
 }
 
 function ShowImage() {
   console.log("Showed image");
   modal.classList.add("showIt");
   modal.classList.remove("hideIt");
+}
+
+function HideImage() {
+  console.log("Hid image");
+  modal.classList.remove("showIt");
+  modal.classList.add("hideIt");
 }
