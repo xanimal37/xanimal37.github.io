38d298179520b31b653aef1a8d329816593a4021
xanimal <zeeb0002@umn.edu>
2019-12-03 11:18:47 -0600

    js

--- js/PicPicker.js
+++ js/PicPicker.js
@@ -2,8 +2,8 @@ var items;
 
 window.onload = function picLoad() {
     console.log("Hello world");
-    items = document.getElementsByClassName("item");
+    items = document.getElementsByClassName("thumb");
     for(var i=0;i<items.length;i++){
-      console.log(items[i].innerHTML);
+      console.log(items[i].alt);
     }
 }
