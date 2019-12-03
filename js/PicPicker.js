7165c5f630c767bcf1048dbee176f3d4f587c875
xanimal <zeeb0002@umn.edu>
2019-12-03 11:13:25 -0600

    Update PicPicker.js

--- js/PicPicker.js
+++ js/PicPicker.js
@@ -3,4 +3,7 @@ var items;
 window.onload = function picLoad() {
     console.log("Hello world");
     items = document.getElementsByClassName("item");
+    for(var i=0;i<items.length;i++){
+      console.log(items[i].name);
+    }
 }
