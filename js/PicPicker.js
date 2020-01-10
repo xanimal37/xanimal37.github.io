46ca9727ebcf77a7135c6329ce125069f79f843a
xanimal <zeeb0002@umn.edu>
2020-01-10 12:31:22 -0600

    fdsafsdaf
    fdasfsdf

--- js/PicPicker.js
+++ js/PicPicker.js
@@ -4,8 +4,6 @@ var imageToShow;
 
 window.onload = function picLoad() {
 
-  console.log("js ok");
-
     modal=document.getElementById("modalWindow");
     imageToShow=document.getElementById("selectedImage");
 
