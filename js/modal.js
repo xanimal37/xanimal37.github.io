45fde45453dbbcd4573da8656e4e33b13dc4a456
LZeeb <zeeb0002@umn.edu>
2020-12-07 15:00:56 -0600

    zoom

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+var modal;
+
+window.onload = function() { 
+  
+    console.log(window.innerWidth);
+    if(window.innerWidth>1199){
+        modal = document.getElementById("Modal");
+        AddAllListeners();
+    }
+} 
+
+function AddAllListeners(){
+
+    var images = document.getElementsByClassName("art");
+
+    for(var i=0;i<images.length;i++)
+        {
+            images[i].addEventListener("click",function(e){
+
+                   document.getElementById("displayImage").src=e.target.src;
+                   modal.classList.remove('hidden');
+                   modal.classList.add('showing');
+            });
+        }
+
+    document.getElementById("CloseButton").addEventListener("click",function(){
+
+        modal.classList.add('hidden');
+        modal.classList.remove('showing');
+        
+    });
+
+}
