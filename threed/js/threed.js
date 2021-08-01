af181ca8c3726b0fc37221ea9f1996f7d0c75d89
xanimal <zeeb0002@umn.edu>
2021-08-01 12:12:11 -0500

    back to min

--- threed/js/threed.js
+++ threed/js/threed.js
@@ -1,15 +1,9 @@
-import * as THREE from 'http://nmgc.umn.edu/threeJS/three.module.js';
-
-class ThreeD {
-    constructor(txt){
-        this.txt=txt;
+class Molecule {
+    constructor(){
+        
     }
 
-    sayHi(){
-        console.log(self.txt);
-    }
+ 
 }
    
     
-threed = new ThreeD('Hello World');
-threed.sayHi();
