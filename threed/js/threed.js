dac3aac8816d191fab2869056523bb46e3ee0cdf
xanimal <zeeb0002@umn.edu>
2021-08-01 12:16:50 -0500

    Update threed.js

--- threed/js/threed.js
+++ threed/js/threed.js
@@ -1,6 +1,9 @@
 class Molecule {
     constructor(){
-        
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth,window.innerHeight);
     }
 
  
