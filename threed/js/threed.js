4def252a44466a0a51db075e177f5d1db4e03d4b
xanimal <zeeb0002@umn.edu>
2021-08-01 11:08:21 -0500

    Update threed.js

--- threed/js/threed.js
+++ threed/js/threed.js
@@ -1,5 +1,15 @@
 import * as THREE from 'library/three.module.js';
 
 class ThreeD {
-    constructor()
+    constructor(txt){
+        self.txt=txt;
+    }
+
+    //method
+    sayHi(){
+        console.log(self.txt);
+    }
 }
+
+threed = new ThreeD('Hello World');
+threed.sayHi();
