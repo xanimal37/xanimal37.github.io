e624a263e2710b0db11e2ff18de97d40ce1252e2
xanimal <zeeb0002@umn.edu>
2021-08-01 17:56:02 -0500

    Update threed.js

--- threed/js/threed.js
+++ threed/js/threed.js
@@ -9,7 +9,7 @@ class Molecule {
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75,w/h,0.1,1000);
-        this.camera.position.z=3;
+        this.camera.position.z=10;
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(w,h);
         this.renderer.setPixelRatio(canvas.devicePixelRatio );
